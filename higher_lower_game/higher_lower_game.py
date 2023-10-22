b0084784b2a97e4008fe8879f7eea104c0d8cf65
Joshua Hecker <jhecker2001@gmail.com>
2023-10-22 09:41:09 -0500

    Added intro function and game function. Added functionality for randomly choosing data from game_data.py.

--- higher_lower_game/higher_lower_game.py
+++ higher_lower_game/higher_lower_game.py
@@ -1,3 +1,16 @@
 import art
 import game_data
 import random
+
+def intro():
+    """Displays intro ASCII art"""
+    print(art.logo)
+    
+def game():
+    playing = True
+    while playing:
+        intro()
+        compare_a = random.choice(game_data.data)
+        compare_b = random.choice(game_data.data)
+        while compare_b == compare_a:
+            compare_b = random.choice(game_data.data)
